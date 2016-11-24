bef541bb9f0f014747af94a071b5ad11aef53820
Vlad Dubrovskis <vlad@vovka.co.uk>
2016-11-24 11:09:18 +0000

    redux store - multiple actions

--- src/state/store.spec.js
+++ src/state/store.spec.js
@@ -4,26 +4,61 @@ import expect from 'expect';
 describe('store', () => {
   it('should work with a series of actions', () => {
 
-    store.dispatch({
-      type: 'ADD_QUOTE_BY_ID',
-      payload: {
-        text: 'The best way to cheer yourself up is to try to cheer somebody else up.',
-        author: 'Mark Twain',
-        id: 1,
-        likeCount: 24
+    const actions = [
+      {
+        type: 'ADD_QUOTE_BY_ID',
+        payload: {
+          text: 'The best way to cheer yourself up is to try to cheer somebody else up.',
+          author: 'Mark Twain',
+          id: 1,
+          likeCount: 24
+        }
+      },
+      {
+        type: 'ADD_QUOTE_BY_ID',
+        payload: {
+          text: 'Whatever you are, be a good one.',
+          author: 'Abraham Lincoln',
+          id: 2,
+          likeCount: 0
+        }
+      },
+      {
+        type: 'REMOVE_QUOTE_BY_ID',
+        payload: {id: 1}
+      },
+      {
+        type: 'LIKE_QUOTE_BY_ID',
+        payload: {id: 2}
+      },
+      {
+        type: 'LIKE_QUOTE_BY_ID',
+        payload: {id: 2}
+      },
+      {
+        type: 'UNLIKE_QUOTE_BY_ID',
+        payload: {id: 2}
+      },
+      {
+        type: 'UPDATE_THEME_COLOR',
+        payload: {color: '#777777'}
       }
-    });
+    ];
+
+    actions.forEach(action => store.dispatch(action));
 
     const actual = store.getState();
     const expected = {
-      quotes: [{
-        text: 'The best way to cheer yourself up is to try to cheer somebody else up.',
-        author: 'Mark Twain',
-        id: 1,
-        likeCount: 24
-      }],
+      quotes: [
+        {
+          text: 'Whatever you are, be a good one.',
+          author: 'Abraham Lincoln',
+          id: 2,
+          likeCount: 1
+        }
+      ],
       theme: {
-        color: "#5DC4C6"
+        color: '#777777'
       }
     };
 
