888de4dc514c22b151d7b5dbe60a9c224af288e6
Vlad Dubrovskis <vlad@vovka.co.uk>
2016-11-24 10:43:05 +0000

    conditional classnames with shallow rendering

--- src/home/quote/components/LikeCounter.spec.js
+++ src/home/quote/components/LikeCounter.spec.js
@@ -11,4 +11,20 @@ describe('LikeCounter', () => {
     const expected = 'a';
     expect(actual).toEqual(expected);
   });
+
+  it('should show the lile count as active', () => {
+    const renderer = TestUtils.createRenderer();
+    renderer.render(<LikeCounter count={5} isActive={true} />);
+    const actual = renderer.getRenderOutput().props.className.includes('LikeCounter--active');
+    const expected = true;
+    expect(actual).toEqual(expected);
+  });
+
+  it('should show the lile count as active', () => {
+    const renderer = TestUtils.createRenderer();
+    renderer.render(<LikeCounter count={5} isActive={false} />);
+    const actual = renderer.getRenderOutput().props.className.includes('LikeCounter--active');
+    const expected = false;
+    expect(actual).toEqual(expected);
+  });
 });
