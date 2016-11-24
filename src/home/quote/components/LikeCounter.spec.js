edeff79219a07633ba6f9306f3c25e2825d6942a
Vlad Dubrovskis <vlad@vovka.co.uk>
2016-11-24 10:56:32 +0000

    children with shallow rendering

--- src/home/quote/components/LikeCounter.spec.js
+++ src/home/quote/components/LikeCounter.spec.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import expect from 'expect';
+import expectJSX from 'expect-jsx';
 import LikeCounter from './LikeCounter';
+
+expect.extend(expectJSX);
 
 describe('LikeCounter', () => {
   it('should be a link', () => {
@@ -10,6 +13,14 @@ describe('LikeCounter', () => {
     const actual = renderer.getRenderOutput().type;
     const expected = 'a';
     expect(actual).toEqual(expected);
+  });
+
+  it('should render like counts', () => {
+    const renderer = TestUtils.createRenderer();
+    renderer.render(<LikeCounter count={5} />);
+    const actual = renderer.getRenderOutput();
+    const expected = '5 likes';
+    expect(actual).toIncludeJSX(expected);
   });
 
   describe('isActive', () => {
